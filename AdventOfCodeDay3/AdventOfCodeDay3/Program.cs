3cb3e18d1c5232eeaaf617f5425c4cea584699e2
Nils Lundell <nils.lundell@dearfriends.se>
2021-12-03 14:24:54 +0100

    Day 3
    Step 2 not attempted

---
+++ AdventOfCodeDay3/AdventOfCodeDay3/Program.cs
@@ -0,0 +1,27 @@
+﻿var inputString = File.ReadAllText(@"C:\Users\nilsl\Documents\Repos\AdventOfCode2021\AdventOfCodeDay3\PuzzleData.txt");
+string[] arr = CleanInputData(inputString);
+
+Console.WriteLine(PuzzleOne(arr));
+
+int PuzzleOne(string[] arr)
+{
+	string gammaRate = "";
+	string epsilonRate = "";
+    for (int i = 0; i < arr[0].Length; i++)
+    {
+		gammaRate += arr.GroupBy(x => x[i])
+			.OrderByDescending(x => x.Count())
+			.Select(x => x).First().Key.ToString();
+
+		epsilonRate += arr.GroupBy(x => x[i])
+			.OrderBy(x => x.Count())
+			.Select(x => x).First().Key.ToString();
+	}
+	return Convert.ToInt32(gammaRate, 2) * Convert.ToInt32(epsilonRate, 2);
+}
+
+string[] CleanInputData(string inputData)
+{
+	string[] arr = inputData.Split(Environment.NewLine);
+	return Enumerable.Range(0, arr.Length).Select(i => arr[i].Trim()).ToArray();
+}
